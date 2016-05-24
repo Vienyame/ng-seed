9702a1a672148b24cbc0e82b800dc38e4e48b5a7
EDORH-TOSSA Viényamé <sergedorh@gmail.com>
2016-05-25 00:09:29 +0200

    browserSync, gulp styles

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,3 +1,11 @@
-/**
- * Created by Serge on 24/05/2016.
- */
+(function() {
+  'use strict';
+
+  var path = require('path');
+  var gulp = require('gulp');
+  var conf = require('./conf');
+
+  var browserSync = require('browser-sync');
+
+
+});
