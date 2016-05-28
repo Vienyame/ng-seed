740889aee580c8f45fe90666fdcf3ab837fec5e8
EDORH-TOSSA Viényamé <sergedorh@gmail.com>
2016-05-28 16:27:01 +0200

    gulp scripts

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -1,3 +1,28 @@
-/**
- * Created by Serge on 24/05/2016.
- */
+(function() {
+  'use strict';
+
+  var path = require('path');
+  var gulp = require('gulp');
+  var conf = require('./conf');
+
+  var browserSync = require('browser-sync');
+
+  var $ = require('gulp-load-plugins')();
+
+  function buildScripts() {
+return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
+  .pipe($.eslint())
+  .pipe($.eslint.format())
+  .pipe($.size())
+  }
+
+
+  gulp.task('scripts-reload', function() {
+    return buildScripts.pipe(browserSync.stream());
+  });
+
+  gulp.task('scripts', function() {
+    return buildScripts();
+  });
+
+})();
