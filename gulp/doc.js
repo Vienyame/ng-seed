020ee0a490ab6e7620e63b61bba595d7c67f51ad
EDORH-TOSSA Viényamé <sergedorh@gmail.com>
2016-05-29 15:27:29 +0200

    install gulp-ngdocs; gulp doc

--- gulp/doc.js
+++ gulp/doc.js
@@ -1,3 +1,39 @@
-/**
- * Created by Serge on 24/05/2016.
- */
+(function(){
+  'use strict';
+
+
+  var path = require('path');
+  var gulp = require('gulp');
+  var conf = require('./conf');
+  var gulpDocs = require('gulp-ngdocs');
+
+  var $ = require('gulp-load-plugins')();
+
+  var wiredep = require('wiredep').stream;
+  var _ = require('lodash');
+
+  var browserSync = require('browser-sync');
+
+  var docsOptions = {
+    scripts: [
+      '../app.min.js'
+    ],
+    html5Mode: true,
+    startPage: '/api',
+    title: "ng-seed documentation",
+    image: "path/to/my/image.png",
+    imageLink: "http://my-domain.com",
+    titleLink: "/api"
+  };
+
+  gulp.task('doc-reload', ['doc'], function() {
+    browserSync.reload();
+  });
+
+  gulp.task('doc', function() {
+    return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
+      .pipe($.ngdocs.process(docsOptions))
+      .pipe(gulp.dest('./docs'));
+  });
+
+})();
